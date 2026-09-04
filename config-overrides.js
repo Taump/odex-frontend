a2c8ba9dbfa2bc93d6787a0a7d245881473fd39a
Alexander Ponomarev <taump@yandex.ru>
2026-09-04 14:49:41 +0200

    feat: rdbuild

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireUglifyjs = require('react-app-rewire-uglifyjs');
-
-module.exports = function override(config, env) {
-  config = rewireReactHotLoader(config, env);
-  config = rewireUglifyjs(config);
-  
-  return config;
-}
